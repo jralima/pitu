99149caea276a77af28a81a4a5242ecc09c657d3
Lacerda Junior <junior.lacerda16@hotmail.com>
2020-11-24 12:07:06 -0300

    Ajustes .env

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pitu
+
 A simple micro-SAAS for URL shortening.
+
+## Documetação:
+
+[](https://github.com/jralima/pitu/blob/master/docs/frontend-para-iniciantes.pdf)
--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const { DB_CONNECTION } = process.env;
-//const sequelize = new Sequelize(DB_CONNECTION as string);
-const sequelize = new Sequelize('mysql://root:jf0705JF@localhost:3306/pitu');
+
+const sequelize = new Sequelize(DB_CONNECTION as string);
 
 export default sequelize;
